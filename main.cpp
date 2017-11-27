a4e4b73674812b2ccff5f59a2321b302e85736a8
Alexander Gudmuundsson <alexandergud18@gmail.com>
2017-11-27 14:25:17 +0000

    read is working -ish

--- main.cpp
+++ main.cpp
@@ -1,39 +1,72 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdlib.h>
 
 using namespace std;
 
-
-void readlinefromfile(ifstream fin);
+bool readlinefromfile(ifstream &fin);
+void read();
+void menu();
 
 int main()
 {
-    ifstream fin;
-    fin.open("textFile.txt");
-
-    char more;
-    do {
-        for (int i = 0; i < 10; i++) {
-            readlinefromfile(fin);
-        }
-        cout << "continue (y/n)?";
-        cin >> more;
-        while(more != 'n' && more != 'y')
-        {
-            cout << "continue (y/n)?";
-            cin >> more;
-        }
-    } while (more != 'n');
+    menu();
     return 0;
 }
 
-void readlinefromfile(ifstream fin)
+void menu()
 {
+    char option;
+
+    cout << "What would you like to do?" << endl;
+    cout << "r - read" << endl;
+    cout << "w - write" << endl;
+    cout << "q - quit" << endl;
+    cin >> option;
+    switch(option) {
+        case 'r':
+            read();
+            break;
+        case 'w':
+            break;
+        case 'q':
+            break;
+    }
+}
+
+void read()
+{
+    ifstream fin;
+    bool done = false;
+    char more;
+    fin.open("textFile.txt");
+
+    do {
+        for (int i = 0; i < 10; i++) {
+            done = readlinefromfile(fin);
+            if(done)
+                break;
+        }
+        cout << "continue (y/n)?";
+        cin >> more;
+        while(more != 'n' && more != 'y') {
+            cout << "continue (y/n)?";
+            cin >> more;
+        }
+    } while (more != 'n' && done != true);
+    system("cls");
+    menu();
+}
+
+bool readlinefromfile(ifstream &fin)
+{
+
     string str;
     if(fin.is_open()) {
         if(fin.eof()) {
             fin.close();
+            return true;
         }
         else {
             getline(fin, str);
@@ -43,4 +76,6 @@ void readlinefromfile(ifstream fin)
     else {
         cout << "Unable to read from file!" << endl;
     }
+
+    return false;
 }
